1458f09c139f7f2e991967b8f9c1daa90068eaf8
PittnoiR <omargarza394@gmail.com>
2024-10-16 12:24:56 -0600

    agregando reactividad Conttec.jsx

--- src/components/Conttec.jsx
+++ src/components/Conttec.jsx
@@ -26,7 +26,6 @@ const technology = [
       "description": "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained."
     }
   ]
-const cambios = null;
 export const Conttec = ()=>{
   const [filters,setFilter] = useState(technology[0])
 
@@ -53,29 +52,24 @@ export const Conttec = ()=>{
     return(
         <section className="h-[88%] w-full overflow-y-scroll  text-center  flex flex-col items-center">
             <h3 className="text-white text-center w-auto  text-[1.2rem] [font-weigth:300]  [font-family:var(--Barlow)] uppercase [letter-spacing:.2rem] mt-4 mb-20"> <strong className="text-gray-400 mr-2">03</strong>Space Launch 101</h3>
-            <figure className="min-h-[32%] w-full">
-                <img className="h-full w-full" src="/assets/technology/image-launch-vehicle-landscape.jpg" alt="" />
+            <figure className="min-h-[37%] w-full">
+                <img className="h-full w-full" src={filters.images.landscape} alt="" />
             </figure>
             <section className="min-h-[525px] w-full px-6 flex flex-col items-center justify-evenly ">
-                <figure className="flex flex-row items-center justify-center  gap-3 text-white text-xl">
-                   <div className="h-12 w-12 rounded-full border-gray-400 border grid place-items-center  hover:border-white transition duration-300 " onClick={cambios} >1</div>
+                <figure className="flex flex-row items-center justify-center  gap-4 text-white text-xl">
+                   <div className="h-12 w-12 rounded-full border-gray-400 border grid place-items-center  hover:border-white transition duration-300 btonActive" onClick={cambios} >1</div>
                    <div className="h-12 w-12 rounded-full border-gray-400 border grid place-items-center hover:border-white transition duration-300 " onClick={cambios}>2</div>
                    <div className="h-12 w-12 rounded-full border-gray-400 border grid place-items-center hover:border-white transition duration-300" onClick={cambios}>3</div> 
                 </figure>
                 <div className="h-auto w-full text-white ">
                     <h6 className="text-xl  text-gray-400 uppercase mb-5 ">the terminology...</h6>
-                    <h3 className="text-3xl text-white  uppercase mb-7">Launch vehicle</h3>
+                    <h3 className="text-3xl text-white  uppercase mb-7">{filters.name}</h3>
                     <p className="[font-family:var(--Barlow)] w-full h-auto text-[1rem] parrafo" >
-                    A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a 
-                    payload from Earth's surface to space, usually to Earth orbit or beyond. Our 
-                    WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, 
-                    it's quite an awe-inspiring sight on the launch pad!
+                      {filters.description}
                     </p>
                 </div>
 
             </section>
-        
-        
         </section>
     )
 
